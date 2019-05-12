f85d925ce383e8f1b2038f6aeb566d6f8a892d61
熊大 <brown@google.com>
2019-05-13 01:26:25 +0800

    20190512 *make:model,factory. migration and tinker to db

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Lesson::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'free' => $faker->boolean()
+    ];
+});
